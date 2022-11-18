08dac6864f0ddf7da2a18c060bfabe160e18773f
Juliana <julianamq20@gmail.com>
2022-11-18 16:51:03 -0300

    alteração no connection

--- src/models/connection.js
+++ src/models/connection.js
@@ -4,7 +4,7 @@ const connection = mysql.createPool({
   host: process.env.MYSQL_HOST || 'localhost',
   port: process.env.MYSQL_PORT || 3306,
   user: process.env.MYSQL_USER || 'root',
-  password: process.env.MYSQL_PASS || '1509',
+  password: process.env.MYSQL_PASSWORD || '1509',
   database: 'StoreManager',
 });
 module.exports = connection;
