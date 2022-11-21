f0f53b6f50c8a789510cfecfc574a604fd430f9c
Juliana <julianamq20@gmail.com>
2022-11-21 18:43:19 -0300

    model

--- src/models/salesModel.js
+++ src/models/salesModel.js
@@ -1,17 +1,15 @@
 const connection = require('./connection');
 
-const getSales = async () => {
-  console.log('test');
-  const [result] = await connection.execute(
-    `SELECT ss.id AS saleId, ss_p.product_id AS productId, ss_p.quantity, ss.date
-    FROM StoreManager.sales_products AS ss_p 
-    INNER JOIN StoreManager.sales AS ss ON ss.id = ss_p.sale_id
-    ORDER BY ss.id, productId;`,
-  );
-  console.log(result);
-  return result;
-};
+const createSales = async () => {
+  const [{ insertId }] = await connection.execute(
+    'INSERT INTO StoreManager.sales () VALUES ()',
 
-module.exports = {
-  getSales,
+  );
+  return insertId;
 };
+module.exports = {
+  createSales,
+};
+// criar 
+// validar 
+// registrar
