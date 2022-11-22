cd4b04d4a79fbab13e5b98e34cf25783917c6275
Juliana <julianamq20@gmail.com>
2022-11-22 08:38:23 -0300

    SalesModel

--- src/models/salesModel.js
+++ src/models/salesModel.js
@@ -7,8 +7,34 @@ const createSales = async () => {
   );
   return insertId;
 };
+const createRegister = async (saleId, productId, quantity) => {
+  await connection.execute(
+    'INSERT INTO StoreManager.sales_products (sale_id, product_id, quantity) VALUES(?, ?, ?)',
+    [saleId, productId, quantity],
+  );
+};
+const deleteProduct = async (id) => {
+  const [{ affectedRows }] = await connection.execute(
+    'DELETE FROM StoreManager.products WHERE id = ?',
+    [id],
+  );
+  return affectedRows;
+};
+
+const validateIds = async () => {
+  const [getValidIds] = await connection.execute(
+    'SELECT id FROM StoreManager.products',
+  );
+
+  const arrayValid = getValidIds.map((item) => item.id);
+  return arrayValid;
+};
+
 module.exports = {
   createSales,
+  createRegister,
+  validateIds,
+  deleteProduct,
 };
 // criar 
 // validar 
